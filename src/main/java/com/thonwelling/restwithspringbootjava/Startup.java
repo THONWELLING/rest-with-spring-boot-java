1b85fddba63431a0491d32739c15c14ce6f06de6
THONWELLING <thonwellingdani13@gmail.com>
2022-12-28 14:32:30 -0300

    Creating first class

--- src/main/java/com/thonwelling/restwithspringbootjava/Startup.java
+++ src/main/java/com/thonwelling/restwithspringbootjava/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestWithSpringBootJavaApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestWithSpringBootJavaApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
