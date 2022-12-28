f6f63d057ea9f7c56ab6f2d6e6cf81f2d88d146d
THONWELLING <thonwellingdani13@gmail.com>
2022-12-28 14:04:35 -0300

    Project Created

---
+++ src/main/java/com/thonwelling/restwithspringbootjava/RestWithSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.thonwelling.restwithspringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thonwelling/restwithspringbootjava/RestWithSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thonwelling.restwithspringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
