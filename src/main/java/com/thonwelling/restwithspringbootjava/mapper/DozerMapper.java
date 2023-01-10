9ec69e355899965707ef8bd47babae13acebee2b
THONWELLING <thonwellingdani13@gmail.com>
2023-01-09 22:00:40 -0300

    Add a mini documentation about dozer

--- src/main/java/com/thonwelling/restwithspringbootjava/mapper/DozerMapper.java
+++ src/main/java/com/thonwelling/restwithspringbootjava/mapper/DozerMapper.java
@@ -5,7 +5,18 @@ import com.github.dozermapper.core.Mapper;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * O Dozer garante que os seus objetos de domínio interno não sejam vistos pelas camadas de apresentação ou por
+ * consumidores externos. Ele também pode ajudar a mapear os seus objetos de domínio para chamadas externas de API’s
+ * e vice-versa.
+ * Em outras palavras, ao invés de você expor as entidades JPA, por exemplo, o que tornaria indiretamente públicas
+ * informações da sua base de dados, cria um POJO similar às suas entidades JPA, contudo sem as anotações,
+ * e expõe esse POJO para os clientes da sua aplicação. Entre o POJO exposto e a entidade JPA poderia utilizar
+ * um framework como o Dozer e parsear dados das suas entidades JPA para o POJO exposto e vice-versa, encapsulando
+ * informações importantes da sua aplicação.
+ *
+ * @author Thonwelling
+ * */
 public class DozerMapper {
   private static final Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
