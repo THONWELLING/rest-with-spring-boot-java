729b854c2ef0ad9c62f9c2f48380d1514566c05c
THONWELLING <thonwellingdani13@gmail.com>
2023-01-19 00:53:00 -0300

    Creating  test to findById method using Mockito

---
+++ src/test/java/com/thonwelling/restwithspringbootjava/services/PersonServiceTest.java
@@ -0,0 +1,79 @@
+package com.thonwelling.restwithspringbootjava.services;
+
+import com.thonwelling.restwithspringbootjava.models.Person;
+import com.thonwelling.restwithspringbootjava.repositories.PersonRepository;
+import com.thonwelling.unittests.mapper.mocks.MockPerson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServiceTest {
+
+  MockPerson input;
+
+  @InjectMocks
+  private PersonService service;
+  @Mock
+  PersonRepository personRepository;
+  @BeforeEach
+  void setUpMocks() throws Exception {
+    input = new MockPerson();
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void getPeopleList() {
+  }
+
+  @Test
+  void getPersonById() throws Exception {
+    Person entity = input.mockEntity(1);
+    entity.setId(1L);
+
+    when(personRepository.findById(1L)).thenReturn(Optional.of(entity));
+
+    var result = service.getPersonById(1L);
+    assertNotNull(result);
+    assertNotNull(result.getKey());
+    assertNotNull(result.getLinks());
+    System.out.println(result.toString());
+    assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+    assertEquals("Addres Test1", result.getAddress());
+    assertEquals("First Name Test1", result.getFirstName());
+    assertEquals("Last Name Test1", result.getLastName());
+    assertEquals("Female", result.getGender());
+  }
+
+  @Test
+void createPerson() {
+    Person entity = input.mockEntity(1);
+    entity.setId(1L);
+
+    when(personRepository.findById(1L)).thenReturn(Optional.of(entity));
+
+  }
+
+  @Test
+  void createPersonV2() {
+  }
+
+  @Test
+  void updatePerson() {
+  }
+
+  @Test
+  void deletePersonById() {
+  }
+}
