92d4cd8483a8b192dcced036eb6a5339e2c1db3f
THONWELLING <thonwellingdani13@gmail.com>
2023-02-04 16:29:37 -0300

    Preparing Testing Infrastructure With Testcontainers

---
+++ src/test/java/com/thonwelling/restwithspringbootjava/configs/IntegrationTestConfig.java
@@ -0,0 +1,5 @@
+package com.thonwelling.restwithspringbootjava.configs;
+
+public class IntegrationTestConfig {
+  public static final int SERVER_PORT = 8888;
+}
---
+++ src/test/java/com/thonwelling/restwithspringbootjava/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.thonwelling.restwithspringbootjava.integrationtests.testcontainers;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+@ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
+public class AbstractIntegrationTest {
+
+  public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>("postgresql:15.1");
+
+    private static void  startContainers(){
+      Startables.deepStart(Stream.of(postgresql)).join();
+    }
+    private static Map<String, String> createConnectionConfiguration() {
+      return Map.of(
+          "spring.datasource.url", postgresql.getJdbcUrl(),
+          "spring.datasource.username", postgresql.getUsername(),
+          "spring.datasource.password", postgresql.getPassword()
+      );
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+      startContainers();
+      ConfigurableEnvironment enviroment = applicationContext.getEnvironment();
+      MapPropertySource testcontainers = new MapPropertySource(
+          "testcontainers", (Map) createConnectionConfiguration());
+      enviroment.getPropertySources().addFirst(testcontainers);
+    }
+
+  }
+}
