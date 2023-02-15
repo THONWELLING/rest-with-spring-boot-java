c553d188906a6dd591e4fb32e521e3cae1e7bef1
THONWELLING <thonwellingdani13@gmail.com>
2023-02-14 23:39:05 -0300

    Add Spring Security, Jwt And Mapping The Entities

--- src/main/java/com/thonwelling/restwithspringbootjava/models/Permission.java
+++ src/main/java/com/thonwelling/restwithspringbootjava/models/Permission.java
@@ -1,2 +1,56 @@
-package com.thonwelling.restwithspringbootjava.models;public class Permission {
+package com.thonwelling.restwithspringbootjava.models;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "permision")
+public class Permission implements GrantedAuthority, Serializable {
+  @Serial
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String description;
+
+  public Permission() {}
+
+  @Override
+  public String getAuthority() {
+    return this.description;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Permission that)) return false;
+    return Objects.equals(getId(), that.getId()) && Objects.equals(getDescription(), that.getDescription());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getDescription());
+  }
 }
