c354bd0d623fd1febc8e433d82c0b673ee6d3151
THONWELLING <thonwellingdani13@gmail.com>
2023-06-13 22:57:53 -0300

    Starting To Dockerizing Application

--- src/main/java/com/thonwelling/restwithspringbootjava/controllers/FileContoller.java
+++ src/main/java/com/thonwelling/restwithspringbootjava/controllers/FileContoller.java
@@ -3,11 +3,13 @@ package com.thonwelling.restwithspringbootjava.controllers;
 import com.thonwelling.restwithspringbootjava.data.dto.v1.UploadFileReponseDTO;
 import com.thonwelling.restwithspringbootjava.services.FileStorageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,7 +26,6 @@ public class FileContoller {
 
   @Autowired
   private FileStorageService service;
-
   @PostMapping("/uploadFile")
   public UploadFileReponseDTO uploadFile(@RequestParam("file")MultipartFile file) {
     logger.info("Storing File To Disc!!");
@@ -38,5 +39,27 @@ public class FileContoller {
   public List<UploadFileReponseDTO> uploadMultipleFiles(@RequestParam("files")MultipartFile[] files) {
     logger.info("Storing Files To Disc!!");
   return Arrays.stream(files).map(this::uploadFile).collect(Collectors.toList());
+  }
+
+  @GetMapping("/downloadFile/{filename:.+}")
+  public ResponseEntity<Resource> downloafFile(@PathVariable String filename, HttpServletRequest request) {
+    logger.info("Reading A File On Disc!!");
+
+    Resource resource = service.loadFileAsResource(filename);
+    String contentType = "";
+    try{
+      contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+    } catch (Exception e){
+      logger.info("Could Not Dtermine File Type");
+    }
+    if (contentType.isBlank()){
+      contentType = "application/octet-stream";
+    }
+
+    return ResponseEntity.ok()
+        .contentType(MediaType.parseMediaType(contentType))
+        .header(HttpHeaders.CONTENT_DISPOSITION,
+            "attachment; filename=\"" + resource.getFilename() + "\"")
+        .body(resource);
   }
 }
--- src/main/java/com/thonwelling/restwithspringbootjava/services/FileStorageService.java
+++ src/main/java/com/thonwelling/restwithspringbootjava/services/FileStorageService.java
@@ -2,11 +2,15 @@ package com.thonwelling.restwithspringbootjava.services;
 
 import com.thonwelling.restwithspringbootjava.config.FileStorageConfig;
 import com.thonwelling.restwithspringbootjava.exceptions.FileStorageException;
+import com.thonwelling.restwithspringbootjava.exceptions.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -43,4 +47,14 @@ public class FileStorageService {
     }
   }
 
+  public Resource loadFileAsResource(String filename) {
+    try {
+      Path filePath = this.fileStorgeLocation.resolve(filename).normalize();
+      Resource resource = new UrlResource(filePath.toUri());
+      if (resource.exists()) return resource;
+      else throw new MyFileNotFoundException("File Not Found");
+    } catch(Exception e){
+      throw new MyFileNotFoundException("File Not Found " + filename, e);
+    }
+  }
 }
